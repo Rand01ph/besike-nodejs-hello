c722ab087091e18cd78157267be0640c3d37ac50
Rand01ph <361212419@qq.com>
2015-05-06 15:16:12 +0800

    setting up mocha for testing

---
+++ test/greet_spec.js
@@ -0,0 +1,9 @@
+var greet = require("greet");
+describe('greet', function(){
+  it("should greet a person by name", function(){
+    expect(greet("world")).to.eql("hello, world");
+  });
+  it("should greet a person flirtatiously if drunk", function(){
+  expect(greet("world", "--drunk")).to.eql("hello world, you look sexy today");
+  });
+});
